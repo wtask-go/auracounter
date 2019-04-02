f33830799ac2a2ca088c527dc3e963b76d6f81f3
Mike Mikhailov <webtask@gmail.com>
2019-04-02 23:06:43 +0300

    add errors dependency

--- internal/logging/doc.go
+++ internal/logging/doc.go
@@ -1,8 +1,8 @@
 /*
-	Package logging provides a logging interface used in the application
-	and some implementations of this interface.
+Package logging provides a logging interface used in the application
+and some implementations of this interface.
 
-	It allows you to isolate the application from external dependencies
-	and postpone the choice of logging libraries for later.
+It allows you to isolate the application from external dependencies
+and postpone the choice of logging libraries for later.
 */
 package logging
