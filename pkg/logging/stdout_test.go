23e238023166572d8d84e9020114e7145f0d2262
Mike Mikhailov <webtask@gmail.com>
2019-04-10 18:26:51 +0300

    logging tests

--- pkg/logging/stdout_test.go
+++ pkg/logging/stdout_test.go
@@ -79,3 +79,27 @@ func ExampleNewStdout_customDecoration() {
 	// ! [2006-01-02 15:04:05.000000] error #2 occurred
 	//
 }
+
+func ExampleNewStdout_asFacade() {
+	logger := NewStdout(
+		WithDecoration(
+			// constant zero-time expected
+			DefaultVerbosity("test", func() time.Time { return time.Time{} }),
+		),
+	)
+	defer logger.Close()
+
+	func(f Facade) {
+		f.Infof("event-1 occurred")
+		f.Infof("event #%d occurred", 2)
+		f.Errorf("error-1 occurred")
+		f.Errorf("error #%d occurred", 2)
+	}(logger)
+
+	// Output:
+	// *test *0001-01-01 00:00:00.000000 *INFO :event-1 occurred
+	// *test *0001-01-01 00:00:00.000000 *INFO :event #2 occurred
+	// *test *0001-01-01 00:00:00.000000 *ERR :error-1 occurred
+	// *test *0001-01-01 00:00:00.000000 *ERR :error #2 occurred
+	//
+}
